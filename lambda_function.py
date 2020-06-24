833f11ee86b35ea5389865474a91c279bc4353a0
Taylor Brodarick <tbrodarick@gmail.com>
2020-06-23 21:31:14 -0500

    Add files via upload
    lambda code - 6/23 classwork

---
+++ lambda_function.py
@@ -0,0 +1,254 @@
+### Required Libraries ###
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from botocore.vendored import requests
+
+from mymodule2 import DoSomething, DoSomethingElse
+
+
+### Functionality Helper Functions ###
+def parse_float(n):
+    """
+    Securely converts a non-numeric value to float.
+    """
+    try:
+        return float(n)
+    except ValueError:
+        return float("nan")
+
+
+def validate_data(intent_request):
+    """
+    Validates the data provided by the user.
+    """
+
+    # # Validate that the user is over 21 years old
+    # if birthday is not None:
+    #     birth_date = datetime.strptime(birthday, "%Y-%m-%d")
+    #     age = relativedelta(datetime.now(), birth_date).years
+    #     if age < 21:
+    #         return build_validation_result(
+    #             False,
+    #             "birthday",
+    #             "You should be at least 21 years old to use this service, "
+    #             "please provide a different date of birth.",
+    #         )
+
+    # A True results is returned if age or amount are valid
+    return build_validation_result(True, None, None)
+
+
+def build_validation_result(is_valid, violated_slot, message_content):
+    """
+    Defines an internal validation message structured as a python dictionary.
+    """
+    if message_content is None:
+        return {"isValid": is_valid, "violatedSlot": violated_slot}
+
+    return {
+        "isValid": is_valid,
+        "violatedSlot": violated_slot,
+        "message": {"contentType": "PlainText", "content": message_content},
+    }
+
+
+### Dialog Actions Helper Functions ###
+def get_slots(intent_request):
+    """
+    Fetch all the slots and their values from the current intent.
+    """
+    return intent_request["currentIntent"]["slots"]
+
+
+def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
+    """
+    Defines an elicit slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "ElicitSlot",
+            "intentName": intent_name,
+            "slots": slots,
+            "slotToElicit": slot_to_elicit,
+            "message": message,
+        },
+    }
+
+
+def delegate(session_attributes, slots):
+    """
+    Defines a delegate slot type response.
+    """
+
+    return {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {"type": "Delegate", "slots": slots},
+    }
+
+
+def close(session_attributes, fulfillment_state, message):
+    """
+    Defines a close slot type response.
+    """
+
+    response = {
+        "sessionAttributes": session_attributes,
+        "dialogAction": {
+            "type": "Close",
+            "fulfillmentState": fulfillment_state,
+            "message": message,
+        },
+    }
+
+    return response
+
+
+### Intents Handlers ###
+def do_something(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    # Gets slots' values
+    slot1 = get_slots(intent_request)["slota"]
+    slot2 = get_slots(intent_request)["slotb"]
+
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
+    source = intent_request["invocationSource"]  #
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+
+        # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]
+                  ] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+
+    # Get the current price of BTC in USD and make the conversion from USD to BTC.
+    retval = DoSomething(slot1, slot2)
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information;
+            {}.
+            """.format(
+                retval
+            ),
+        },
+    )
+
+
+def do_something_else(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    # Gets slots' values
+    slot1 = get_slots(intent_request)["slotc"]
+    slot2 = get_slots(intent_request)["slotd"]
+
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
+    source = intent_request["invocationSource"]  #
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+
+        # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]
+                  ] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+
+    # Get the current price of BTC in USD and make the conversion from USD to BTC.
+    retval = DoSomethingElse(slot1, slot2)
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information;
+            {}.
+            """.format(
+                retval
+            ),
+        },
+    )
+
+
+### Intents Dispatcher ###
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    # Get the name of the current intent
+    intent_name = intent_request["currentIntent"]["name"]
+
+    # Dispatch to bot's intent handlers
+    if intent_name == "DoSomething":
+        return do_something(intent_request)
+    elif intent_name == "DoSomethingElse":
+        return do_something_else(intent_request)
+
+    raise Exception("Intent with name " + intent_name + " not supported")
+
+
+### Main Handler ###
+def lambda_handler(event, context):
+    """
+    Route the incoming request based on intent.
+    The JSON body of the request is provided in the event slot.
+    """
+
+    return dispatch(event)
---
+++ mymodule2.py
@@ -0,0 +1,13 @@
+import json
+
+
+def DoSomething(parm1, parm2):
+    return f"{parm1} is not {parm2}"
+
+
+def DoSomethingElse(parm1, parm2):
+    return f"You like {parm1}?  You must be a {parm2}."
+
+
+def MachineLearningPredictions(tickerSymbol):
+    return "BUY!!!!!"
