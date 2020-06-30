00f95bab2e0a2f7e8576d978c0a73ac115ef466b
Peter Lawson <lawsonpd@gmail.com>
2020-06-30 19:18:51 -0400

    Add volatility.ipynb (although it's not needed and I'll likely remove).

--- WeightSelectionModel/predict.py
+++ WeightSelectionModel/predict.py
@@ -1,21 +1,47 @@
 import pandas as pd
 import numpy as np
 import joblib
+from datetime import datetime, timedelta
 
 from pathlib import Path
 import os
 
-class WeightSelector(Object):
-    def __init__(self):
+class WeightSelector():
+    def __init__(self, tickers, period=30):
         # TODO
-        pass
+        self.tickers = tickers
+        try:
+            self.tickers != []
+        except EmptyStockList:
+            print('No stocks provided.')
+        # `period` is how far ahead we want to predict
+        #
+        # Implicit is that we will reevaluate on today+period,
+        # so we're concerned with predicting the return on today+period.
+        self.period = period
+        try:
+            self.period > 0
+        except ZeroPeriod:
+            print('Period must be greater than 1')
+        self.today = datetime.today()
 
     def __repr__(self):
         # TODO
         # return self.clf.summary()
         pass
 
-class TestModels(Object):
+class TestModels():
     def __init__(self, tickers, period):
         self.tickers = tickers
         self.period = period
+
+def get_trained_model():
+    # TODO
+    pass
+
+def test():
+    ws1 = WeightSelector(['AAPL', 'AMZN', 'NFLX'], 10)
+    ws2 = WeightSelector([])
+
+if __name__ == '__main__':
+    test()
