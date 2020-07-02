2021c8a88194f9b8c2f6899ab333a384f3639559
CPix <39272924+CPix18@users.noreply.github.com>
2020-07-02 12:48:08 -0500

    Early Thursday Lambda update

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -1,9 +1,7 @@
 ### Required Libraries ###
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from botocore.vendored import requests
 
-from mymodule2 import DoSomething, DoSomethingElse
+from botocore.vendored import requests
+# from mymodule2 import userProfile, filterIntent, selectorIntent, defineIntent, MLIntent
 
 
 ### Functionality Helper Functions ###
@@ -21,18 +19,6 @@ def validate_data(intent_request):
     """
     Validates the data provided by the user.
     """
-
-    # # Validate that the user is over 21 years old
-    # if birthday is not None:
-    #     birth_date = datetime.strptime(birthday, "%Y-%m-%d")
-    #     age = relativedelta(datetime.now(), birth_date).years
-    #     if age < 21:
-    #         return build_validation_result(
-    #             False,
-    #             "birthday",
-    #             "You should be at least 21 years old to use this service, "
-    #             "please provide a different date of birth.",
-    #         )
 
     # A True results is returned if age or amount are valid
     return build_validation_result(True, None, None)
@@ -104,16 +90,16 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
-
 ### Intents Handlers ###
-def do_something(intent_request):
+def userProfile(intent_request):
     """
     Performs dialog management and fulfillment for recommending a portfolio.
     """
 
     # Gets slots' values
-    slot1 = get_slots(intent_request)["slota"]
-    slot2 = get_slots(intent_request)["slotb"]
+    firstName = get_slots(intent_request)["firstName"]
+    userAge = get_slots(intent_request)["userAge"]
+    contactInfo = get_slots(intent_request)["contactInfo"]
 
     # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
     source = intent_request["invocationSource"]  #
@@ -147,9 +133,9 @@ def do_something(intent_request):
 
         # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
         return delegate(output_session_attributes, get_slots(intent_request))
-
-    # Get the current price of BTC in USD and make the conversion from USD to BTC.
-    retval = DoSomething(slot1, slot2)
+    
+    intent_request["sessionAttributes"]["userProfileResult"] = {"firstName": firstName, 
+        "userAge": userAge, "contactInfo": contactInfo}
 
     # Return a message with conversion's result.
     return close(
@@ -160,20 +146,85 @@ def do_something(intent_request):
             "content": """Thank you for your information;
             {}.
             """.format(
-                retval
+                "Good user info"
             ),
         },
     )
 
-
-def do_something_else(intent_request):
+def filterIntent(intent_request):
     """
     Performs dialog management and fulfillment for recommending a portfolio.
     """
 
     # Gets slots' values
-    slot1 = get_slots(intent_request)["slotc"]
-    slot2 = get_slots(intent_request)["slotd"]
+    marketCap = get_slots(intent_request)["marketCap"]
+    tradeVolume = get_slots(intent_request)["tradeVolume"]
+    dividendPayer = get_slots(intent_request)["dividendPayer"]
+    sharePrice = get_slots(intent_request)["sharePrice"]
+    priceEarning = get_slots(intent_request)["priceEarning"]
+    priceSale = get_slots(intent_request)["priceSale"]
+    priceBook = get_slots(intent_request)["priceBook"]
+
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
+    source = intent_request["invocationSource"]  #
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+
+        # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]
+                  ] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+        
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+        
+    intent_request["sessionAttributes"]["filterIntentResult"] = {"marketCap": marketCap, 
+        "tradeVolume": tradeVolume, "dividendPayer": dividendPayer,
+        "sharePrice": sharePrice, "priceEarning": priceEarning, 
+        "priceSale": priceSale, "priceBook": priceBook}
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information;
+            {}.
+            """.format(
+                "good filter info"
+            ),
+        },
+    )
+
+def selectorIntent(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    # Gets slots' values
+    bollingerBand = get_slots(intent_request)["bollingerBand"]
+    eMA = get_slots(intent_request)["eMA"]
 
     # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
     source = intent_request["invocationSource"]  #
@@ -207,9 +258,9 @@ def do_something_else(intent_request):
 
         # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
         return delegate(output_session_attributes, get_slots(intent_request))
-
-    # Get the current price of BTC in USD and make the conversion from USD to BTC.
-    retval = DoSomethingElse(slot1, slot2)
+    
+    intent_request["sessionAttributes"]["selectorIntentResult"] = {"bollingerBand": bollingerBand, 
+        "eMA": eMA}
 
     # Return a message with conversion's result.
     return close(
@@ -220,11 +271,126 @@ def do_something_else(intent_request):
             "content": """Thank you for your information;
             {}.
             """.format(
-                retval
+                "good selector info"
             ),
         },
     )
 
+def defineIntent(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    # Gets slots' values
+    timeFrame = get_slots(intent_request)["timeFrame"]
+    portfolioSize = get_slots(intent_request)["portfolioSize"]
+
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
+    source = intent_request["invocationSource"]  #
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+
+        # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]
+                  ] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+    intent_request["sessionAttributes"]["defineIntentResult"] = {"portfolioSize": portfolioSize, 
+        "timeFrame": timeFrame}
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information;
+            {}.
+            """.format(
+                "good define info"
+            ),
+        },
+    )
+
+def MLIntent(intent_request):
+    """
+    Performs dialog management and fulfillment for recommending a portfolio.
+    """
+
+    # Gets slots' values
+    mlPredictions = get_slots(intent_request)["mlPredictions"]
+
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
+    source = intent_request["invocationSource"]  #
+
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots.
+
+        # Gets all the slots
+        slots = get_slots(intent_request)
+
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(intent_request)
+
+        # If the data provided by the user is not valid,
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected.
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]
+                  ] = None  # Cleans invalid slot
+
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action.
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+    intent_request["sessionAttributes"]["defineIntentResult"] = {"mlPredictions": mlPredictions}
+
+    # Return a message with conversion's result.
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information;
+            {}.
+            """.format(
+                "good ML info"
+            ),
+        },
+    )
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
@@ -236,10 +402,16 @@ def dispatch(intent_request):
     intent_name = intent_request["currentIntent"]["name"]
 
     # Dispatch to bot's intent handlers
-    if intent_name == "DoSomething":
-        return do_something(intent_request)
-    elif intent_name == "DoSomethingElse":
-        return do_something_else(intent_request)
+    if intent_name == "userProfile":
+        return userProfile(intent_request)
+    elif intent_name == "filterIntent":
+        return filterIntent(intent_request)
+    elif intent_name == "selectorIntent":
+        return selectorIntent(intent_request)
+    elif intent_name == "defineIntent":
+        return defineIntent(intent_request)
+    elif intent_name == "MLIntent":
+        return MLIntent(intent_request)
 
     raise Exception("Intent with name " + intent_name + " not supported")
 
